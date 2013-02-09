6b085b3ddb49ccba81e52887a267ee4bd5ed0301
Clinton N. Dreisbach <crnixon@gmail.com>
2013-02-09 16:57:31 -0500

    Reflect so that all tables are available through data.tables

--- kenbot/data.py
+++ kenbot/data.py
@@ -2,10 +2,19 @@ from sqlalchemy import Table, MetaData
 
 from . import db
 
-meta = db.MetaData()
+meta = db.metadata
+meta.reflect(bind=db.engine)
 
-CBSA = db.Table('cbsa', db.metadata, autoload=True, autoload_with=db.engine)
-HMDA = db.Table('hmda', db.metadata, autoload=True, autoload_with=db.engine)
-State = db.Table('state', db.metadata, autoload=True, autoload_with=db.engine)
-County = db.Table('county', db.metadata, autoload=True, autoload_with=db.engine)
+tables = db.metadata.tables
 
+connection = None
+
+HMDA = db.Table('hmda', meta, autoload=True, autoload_with=db.engine)
+CBSA = db.Table('cbsa', meta, autoload=True, autoload_with=db.engine)
+State = db.Table('state', meta, autoload=True, autoload_with=db.engine)
+County = db.Table('county', meta, autoload=True, autoload_with=db.engine)
+
+def conn():
+    if connection is None:
+        connection = db.engine.connect()
+    return connection
