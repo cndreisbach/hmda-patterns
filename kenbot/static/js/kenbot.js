dd5a3fef708a78c5ba3eac3cc59be1c96d7bfc0c
Clinton N. Dreisbach <crnixon@gmail.com>
2013-02-10 23:37:16 -0500

    Added denial rates by income chart

--- kenbot/static/js/kenbot.js
+++ kenbot/static/js/kenbot.js
@@ -1,4 +1,11 @@
 (function ($) {
+  var tt = document.createElement('div'),
+      leftOffset = -(~~$('html').css('padding-left').replace('px', '') + ~~$('body').css('margin-left').replace('px', '')),
+      topOffset = -32;
+
+  tt.className = 'ex-tooltip';
+  document.body.appendChild(tt);
+
   var updateDenialRates = function (msa_md) {    
     $.get('/denial_rates_data/' + msa_md, function (data, textStatus, xhr) {
       data = data.result.map(function (datum) {
@@ -14,7 +21,6 @@
       var chartData = {
         "xScale": "ordinal",
         "yScale": "linear",
-        "type": "bar",
         "main": [
           {
             "className": ".msa",
@@ -26,14 +32,6 @@
           }
         ]
       };
-
-      var tt =
-        document.createElement('div'),
-        leftOffset = -(~~$('html').css('padding-left').replace('px', '') + ~~$('body').css('margin-left').replace('px', '')),
-        topOffset = -32;
-
-      tt.className = 'ex-tooltip';
-      document.body.appendChild(tt);
       
       var options = {
         "tickFormatX": function (d) {
@@ -50,12 +48,79 @@
         }
       };
       
-      var firstChart = new xChart('bar',
+      var raceChart = new xChart('bar',
                                   chartData,
-                                  '#first-chart',
+                                  '#race-chart',
                                   options);
     });
   };
+
+  var updateDenialRatesByIncome = function (msa_md) {
+    $.get('/denial_by_income/' + msa_md, function (data, textStatus, xhr) {
+      data = data.result.map(function (datum) {
+        return {
+          "x": datum.income_group,
+          "y": datum.denial_percent,
+          "income_group": datum.income_group,
+          "race": datum.race,
+          "total_denied": datum.total_denied
+        };
+      }).filter(function (datum) {
+        return datum.income_group !== 999999;
+      }).groupBy(function (datum) {
+        return datum.race;
+      });
+
+      var chartData = {
+        "xScale": "linear",
+        "yScale": "linear",
+        "main": [
+          {
+            "className": ".white",
+            "data": data['White']
+          },
+          {
+            "className": ".asian",
+            "data": data['Asian']
+          },
+          {
+            "className": ".black",
+            "data": data['Black or African American']
+          },
+          {
+            "className": ".pacific",
+            "data": data['Native Hawaiian or Other Pacific Islander']
+          },
+          {
+            "className": ".first-nations",
+            "data": data['American Indian or Alaska Native']
+          }
+        ]
+      };
+
+      var options = {
+        "xMin": 30,
+        "xMax": 150,
+        "tickFormatX": function (d) {
+          return "$" + (d * 1000).format();
+        },
+        "mouseover": function (d, i) {
+          var pos = $(this).offset();
+          $(tt).text(d.race + " - " + d.y.round(2) + "%")
+            .css({top: topOffset + pos.top, left: pos.left + leftOffset})
+            .show();
+        },
+        "mouseout": function (x) {
+          $(tt).hide();
+        }
+      };
+
+      var incomeChart = new xChart('line-dotted',
+                                  chartData,
+                                  '#income-chart',
+                                  options);      
+    });
+  }
 
   var activateSketchPad = function(){
       $(function() {
@@ -70,6 +135,7 @@
   };
 
   window.updateDenialRates = updateDenialRates;
+  window.updateDenialRatesByIncome = updateDenialRatesByIncome;
   window.activateSketchPad = activateSketchPad;
 
   $(document).ready(function () {
