6f683448ed6cc4d0200389e03e1e46d78ecadae1
Clinton N. Dreisbach <crnixon@gmail.com>
2013-02-16 07:47:11 -0500

    Refactor data functions

--- kenbot/data.py
+++ kenbot/data.py
@@ -1,15 +1,44 @@
 from flask import g
 
 
-def msas():
+def msas(db=None):
+    db = db or g.db
     return g.db.cbsa.filter(g.db.cbsa.parent_code == None).all()
 
 
-def msa(cbsa_code):
+def msa(cbsa_code, db=None):
+    db = db or g.db
     return g.db.cbsa.filter_by(cbsa_code=cbsa_code).one()
 
 
-def denial_rates(msa_md=None):
+def loan_table(cbsa_code, db=None):
+    db = db or g.db
+    sql = """SELECT s.abbr as state_abbr,
+                    c.name as county_name,
+                    sum(case when h.loan_purpose = 1
+                             then 1
+                             else 0 end) as home_purchase_count,
+                    sum(case when h.loan_purpose = 1 and h.loan_type != 1
+                             then 1
+                             else 0 end) as gbl_home_purchase_count,
+                    sum(case when h.loan_purpose = 3
+                             then 1
+                             else 0 end) as refinance_count,
+                    sum(case when h.loan_purpose = 3 and h.loan_type != 1
+                             then 1
+                             else 0 end) as gbl_refinance_count
+             FROM hmda h
+                 JOIN state s on h.state_code = s.fips_code
+                 JOIN county c on h.state_code = c.state_fips_code AND
+                                  h.county_code = c.county_fips_code
+             WHERE h.msa_md = :cbsa_code
+             GROUP BY s.abbr, c.name"""
+
+    return db.execute(sql, params={'cbsa_code': cbsa_code}).fetchall()
+
+
+def denial_rates(msa_md=None, db=None):
+    db = db or g.db
     sql = """with denials_by_race as (
                 select count(*) as total
                     ,sum(case when action_type = 1 then 1 else 0 end) as approval_count
@@ -30,10 +59,11 @@ def denial_rates(msa_md=None):
         join loan_purpose lp on d.loan_purpose = lp.id
         order by loan_purpose, race"""
 
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
 
 
-def denial_by_income(msa_md=None):
+def denial_by_income(msa_md=None, db=None):
+    db = db or g.db
     sql = """
         select sum(total) as total,
                sum(case when action_type_denied = 1 then total else 0 end) as total_denied,
@@ -52,10 +82,11 @@ def denial_by_income(msa_md=None):
         order by r.race, income_group
     """
 
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
 
 
-def hal_gov_backed_by_income(msa_md=None):
+def hal_gov_backed_by_income(msa_md=None, db=None):
+    db = db or g.db
     sql = """
         select sum(total) as total,
                income_group,
@@ -76,10 +107,11 @@ def hal_gov_backed_by_income(msa_md=None):
         group by income_group
         order by income_group
     """
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
 
 
-def hal_gov_backed_by_race(msa_md=None):
+def hal_gov_backed_by_race(msa_md=None, db=None):
+    db = db or g.db
     sql = """
         select sum(total) as total,
                r.race,
@@ -101,10 +133,11 @@ def hal_gov_backed_by_race(msa_md=None):
         group by  r.race
         order by  r.race
     """
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
 
 
-def gov_backed_by_race_purpose(msa_md=None):
+def gov_backed_by_race_purpose(msa_md=None, db=None):
+    db = db or g.db
     sql = """
         select sum(total) as total,
                case when loan_purpose = 1 then 'purchase' else 'refinance' end as loan_purpose_name,
@@ -121,10 +154,11 @@ def gov_backed_by_race_purpose(msa_md=None):
         group by r.race, loan_purpose
         order by r.race, loan_purpose
     """
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
 
 
-def gov_backed_by_income_purpose(msa_md=None):
+def gov_backed_by_income_purpose(msa_md=None, db=None):
+    db = db or g.db
     sql = """
         select sum(total) as total,
                case when loan_purpose = 1 then 'purchase' else 'refinance' end as loan_purpose_name,
@@ -142,4 +176,4 @@ def gov_backed_by_income_purpose(msa_md=None):
         group by income_group,  loan_purpose
         order by income_group,  loan_purpose
     """
-    return g.db.execute(sql, params={'msa_md': msa_md}).fetchall()
+    return db.execute(sql, params={'msa_md': msa_md}).fetchall()
