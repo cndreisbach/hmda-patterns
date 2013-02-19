27ec4e4da5c0ea089a1144c503040a7783b68c9f
Clinton N. Dreisbach <crnixon@gmail.com>
2013-02-19 16:16:58 -0500

    Add assets command to manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from flask.ext.script import Manager, Server
-
+from flask.ext.assets import ManageAssets
 from kenbot import create_app
 
 manager = Manager(create_app)
@@ -11,6 +11,7 @@ manager.add_option('-c', '--config',
                    help='Path to configuration file.',
                    default='sample_config.py')
 manager.add_command("runserver", Server())
+manager.add_command("assets", ManageAssets())
 
 if __name__ == "__main__":
     manager.run()
